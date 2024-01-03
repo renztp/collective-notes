7afe80f21de94232f6626afb99871ba43f5c49eb
renzpulvira <tullaorenz@gmail.com>
2024-01-04 02:29:21 +0800

    Added changes to include a new form library for editing notes

--- frontend/src/components/pages/collection-notes/collection-single.tsx
+++ frontend/src/components/pages/collection-notes/collection-single.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { FiHeart } from "react-icons/fi";
 import { Link, useLoaderData } from "react-router-dom";
+import Editor from "react-simple-wysiwyg";
 import { Collection } from "./collection";
 import { getCollection } from "./collection.service";
-import { formatDateToMonthDayYear } from "../../../utils/date.utils";
 import { NotesContent } from "./notes-content";
-import { FiHeart } from "react-icons/fi";
 
 export async function loader({ params }: string) {
   const collection = await getCollection(params.collectionId);
@@ -13,32 +13,62 @@ export async function loader({ params }: string) {
 
 export const CollectionSingle = () => {
   const [isNoteEditing, setIsNoteStateEditing] = useState(false);
-  const collection = useLoaderData() as Collection;
+  const collection = useLoaderData() as Collection || "";
 
-  console.log(collection);
-  const date = new Date(collection.createdDate);
-  console.log({ date });
+  const [noteTitle, setNoteTitle] = useState("");
+  const [noteDescription, setNoteDescription] = useState("");
+
+  useEffect(() => {
+    console.log({ noteTitle, noteDescription });
+  }, [noteTitle, noteDescription]);
+
+  useEffect(() => {
+    setNoteTitle(collection.title);
+    setNoteDescription(collection.description);
+  }, [collection]);
+
+  function onSave(e) {
+    e.preventDefault();
+
+    console.log('collection', collection);
+    setIsNoteStateEditing(false);
+  }
 
 
   return (
     <>
       <div className="note-action flex justify-between">
         <Link to="/collections" className="bg-cn-secondary py-2 px-3 rounded-lg border border-white hover:border hover:border-cn-accent">Go back</Link>
-        <div className="flex items-center"><FiHeart className="mr-3 text-2xl cursor-pointer" />{!isNoteEditing ? <ViewButtonGroup changeEditingState={setIsNoteStateEditing} /> : <EditButtonGroup changeEditingState={setIsNoteStateEditing} />}</div>
+        <div className="flex items-center"><FiHeart className="mr-3 text-2xl cursor-pointer" />
+          {
+            !isNoteEditing
+              ? <ViewButtonGroup buttonAction={setIsNoteStateEditing} />
+              : <EditButtonGroup buttonAction={onSave} />
+          }
+        </div>
       </div>
-      { !isNoteEditing ? 
+      {!isNoteEditing ?
         <div>
           {
-            collection ?
-              <NotesContent title={collection.title} description={collection.description} createdDate={collection.createdDate} lastModified={collection.lastModified} />
+            collection
+              ? <NotesContent
+                title={collection.title}
+                description={collection.description}
+                createdDate={collection.createdDate}
+                lastModified={collection.lastModified}
+              />
               :
               <p>Loading...</p>
           }
         </div>
         :
         <div className="w-[80%] m-auto text-center pt-5">
-          <div className="block size-full text-center mb-5"><input className="rounded border border-gray-800 p-3 text-center text-2xl font-bold text-cn-text-secondary mr-3 block w-[100%]" type="text" value={collection.title} /></div>
-          <div className="block size-full"><textarea className="rounded border w-[100%] border-gray-800 p-3" value={collection.description}></textarea></div>
+          <div className="block size-full text-center mb-5"><input onChange={e => setNoteTitle(e.target.value)} className="rounded border border-gray-800 p-3 text-center text-2xl font-bold text-cn-text-secondary mr-3 block w-[100%]" type="text" value={noteTitle} /></div>
+          <div className="block size-full">
+            <Editor value={noteDescription} onChange={(e) => setNoteDescription(e.target.value)} />
+            {/* <textarea className="rounded border w-[100%] border-gray-800 p-3" value={collection.description}> */}
+            {/* </textarea> */}
+          </div>
         </div>
       }
     </>
@@ -46,22 +76,23 @@ export const CollectionSingle = () => {
 };
 
 interface ButtonGroupProps {
-  changeEditingState?: (newState: boolean) => void;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  buttonAction: (state) => void;
 }
 
-const ViewButtonGroup: React.FC<ButtonGroupProps> = ({ changeEditingState }) => {
+const ViewButtonGroup: React.FC<ButtonGroupProps> = ({ buttonAction }) => {
   return (
     <div className="flex justify-between">
-      <button onClick={() => changeEditingState(true)} className="bg-cn-secondary py-2 px-3 rounded-lg border border-white hover:border hover:border-cn-accent">Edit</button>
+      <button onClick={() => buttonAction(true)} className="bg-cn-secondary py-2 px-3 rounded-lg border border-white hover:border hover:border-cn-accent">Edit</button>
     </div>
   );
 };
 
-const EditButtonGroup: React.FC<ButtonGroupProps> = ({ changeEditingState }) => {
+const EditButtonGroup: React.FC<ButtonGroupProps> = ({ buttonAction }) => {
   return (
     <div className="flex justify-between">
       <button className="bg-red-400 text-white py-2 px-3 rounded-lg border border-white hover:border hover:border-cn-accent mr-2">Cancel</button>
-      <button onClick={() => changeEditingState(false)} className="bg-cn-secondary py-2 px-3 rounded-lg border border-white hover:border hover:border-cn-accent">Save</button>
+      <button onClick={(e) => buttonAction(e)} className="bg-cn-secondary py-2 px-3 rounded-lg border border-white hover:border hover:border-cn-accent">Save</button>
     </div>
   );
 };
--- frontend/src/components/pages/collection-notes/collection.service.tsx
+++ frontend/src/components/pages/collection-notes/collection.service.tsx
@@ -2,9 +2,35 @@ const BASE_URL = 'http://localhost:3000/collections';
 import axios from 'axios';
 
 export async function getCollection(collectionId: string) {
-  return await axios(`${BASE_URL}/${collectionId}`).then((res) => res.data);
+  try {
+    const res = await axios(`${BASE_URL}/${collectionId}`);
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function getAllCollections() {
-  return await axios(BASE_URL).then((res) => res.data);
+  try {
+    const res = await axios(BASE_URL);
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+interface NoteData {
+  id: string;
+  title: string;
+  description: string;
+}
+
+export async function updateNote(noteData: NoteData) {
+  try {
+    const res = await axios.put(`${BASE_URL}/${noteData.id}`, noteData);
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
